658b7e43e680e847d54473df4c19ae7f9bfeaf06
junsung-info <kimjsung200012@gmail.com>
2025-03-01 18:50:49 +0900

    edit run.sh

--- run.sh
+++ run.sh
@@ -13,11 +13,12 @@ echo "done for running use-full-precision.py" >> log
 full_end_time=$(date '+%s')
 
 full_diff=$((full_end_time - full_start_time))
+full_day=$((full_diff / (3600 * 24)))
 full_hour=$((full_diff / 3600 % 24))
 full_minute=$((full_diff / 60 % 60))
 full_second=$((full_diff % 60))
 
-echo "Total time: $full_hour h $full_minute m $full_second s" >> log
+echo "Total time: $full_day days $full_hour h $full_minute m $full_second s" >> log
 
 echo "removing ~/.cache/huggingface dir" >> log
 rm -rf ~/.cache/huggingface
@@ -25,6 +26,11 @@ echo "done for removing" >> log
 
 echo "ollama serve" >> log
 ollama serve &
+
+sleep 60
+
+echo "ollama pull deepseek-r1:70b" >> log
+ollama pull deepseek-r1:70b
 
 quantized_start_time=$(date '+%s')
 
@@ -35,11 +41,12 @@ echo "done for running use-quantized.py" >> log
 quantized_end_time=$(date '+%s')
 
 quantized_diff=$((quantized_end_time - quantized_start_time))
+quantized_day=$((quantized_diff / (3600 * 24)))
 quantized_hour=$((quantized_diff / 3600 % 24))
 quantized_minute=$((quantized_diff / 60 % 60))
 quantized_second=$((quantized_diff % 60))
 
-echo "Total time: $quantized_hour h $quantized_minute m $quantized_second s" >> log
+echo "Total time: $quantized_day days $quantized_hour h $quantized_minute m $quantized_second s" >> log
 
 echo "removing deepseek r1 distill llama 70b" >> log
 ollama rm deepseek-r1:70b
@@ -47,10 +54,11 @@ ollama rm deepseek-r1:70b
 end_time=$(date '+%s')
 
 diff=$((end_time - start_time))
+day=$((diff / (3600 * 24)))
 hour=$((diff / 3600 % 24))
 minute=$((diff / 60 % 60))
 second=$((diff % 60))
 
-echo "Total time: $hour h $minute m $second s" >> log
+echo "Total time: $day days $hour h $minute m $second s" >> log
 
 deactivate
